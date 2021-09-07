c299ecb1d21461f7749c0befee3f4d1c40712caa
Ahmet Yılmaz <ahmetyilmaz00@outlook.com.tr>
2021-09-07 14:37:05 +0300

    Add project files.

---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,72 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Thread t = new Thread(new ThreadStart(ChangeText));
+            t.Start();
+        }
+        private void ChangeText()
+        {
+            while (true)
+            {
+                _consumer = new Consumer(_queueName);
+                SetText(_consumer.GetMessage());
+            }
+        }
+        private delegate void SetTextDelegate(string number);
+        private void SetText(string number)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new SetTextDelegate(SetText), new object[] { number });
+                return;
+            }
+            
+            if (number != null)
+            {
+                listBox1.Items.Add("KMS Customer Client: " + number);
+            }       
+        }
+        //it doesnt find queue first running because there is no queue in name 
+        private static readonly string _queueName = "KMSCustomerClient";
+        private static readonly string _queueNameBack = "KMSBaseClient";
+        private static Publisher _publisher;
+        private static Consumer _consumer;
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (sender is TextBox)
+                {
+                    TextBox txb = (TextBox)sender;
+                    _publisher = new Publisher(_queueNameBack, txb.Text);
+                    listBox1.Items.Add("KMS Base Client " + txb.Text);
+                    textBox1.Text = "";
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "KMSBaseClient";
+        }
+    }
+}
---
+++ WinFormsApp2/RabbitMQService.cs
@@ -0,0 +1,71 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2
+{
+    public class RabbitMQService
+    {
+        private readonly string _hostName = "localhost";
+        public IConnection GetRabbitMQConnection()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                HostName = _hostName
+            };
+            return connectionFactory.CreateConnection();
+        }
+    }
+    public class Publisher
+    {
+        private readonly RabbitMQService _rabbitMQService;
+        public Publisher(string queueName, string message)
+        {
+            _rabbitMQService = new RabbitMQService();
+
+            using (var connection = _rabbitMQService.GetRabbitMQConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queueName, false, false, false, null);
+
+                    channel.BasicPublish("", queueName, null, Encoding.UTF8.GetBytes(message));
+
+                    //Console.WriteLine("{0} queue'su üzerine, \"{1}\" mesajı yazıldı.", queueName, message);
+                }
+            }
+        }
+    }
+    public class Consumer
+    {
+        private readonly RabbitMQService _rabbitMQService;
+        private string getMessage { get; set; }
+        public Consumer(string queueName)
+        {
+            _rabbitMQService = new RabbitMQService();
+
+            using (var connection = _rabbitMQService.GetRabbitMQConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        this.getMessage = Encoding.UTF8.GetString(body);
+                    };
+                    channel.BasicCancel(channel.BasicConsume(queueName, true, consumer));
+                    //channel.BasicConsume(queueName, true, consumer);
+                }
+            }
+        }
+        public string GetMessage()
+        {
+            return getMessage;
+        }
+    }
+}
